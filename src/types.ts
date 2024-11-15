ebce3162fb350ed3244b711d99e06189c345a8ec
artemijr <Arrtemij96@gmail.com>
2024-11-16 01:36:14 +0300

    Updated and added types

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,11 @@
 export interface Story {
-    id: string;
+    id: number;
     image: string;
     expiresAt: number;  // Timestamp
 }
 
 export interface Contact {
-    id: string;
+    id: number;
     name: string;
     pfp: string;
     lastMessage?: string;
@@ -15,8 +15,37 @@ export interface Contact {
 export interface Message {
     sender: 'me' | 'them';
     text: string;
+    timestamp?: string;
 }
 
 export interface ChatProps {
     contacts: Contact[];
+}
+
+export interface Person {
+   id: number; // Уникальный идентификатор
+   name: string; // Имя человека
+   description: string; // Описание человека
+   imageUrl: string; // Ссылка на изображение
+}
+
+export interface PersonParams {
+    height?: number;
+    worldview?: string;
+    zodiacSign?: string;
+    children?: string;
+    alcohol?: string;
+    smoking?: string;
+    languages?: string[];
+}
+
+export interface ProfileData {
+    username: string;
+    bio: string;
+    tags: string[];
+    photos: {
+        logo: string;
+        carousel: string[];
+    };
+    person_params: PersonParams;
 }
