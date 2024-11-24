5c613eb02c1e169984aef2d44f8143cccbdbac5e
graevsky <graevsky@bk.ru>
2024-11-24 17:29:27 +0300

    Fix lint

--- tests/__mocks__/react-filerobot-image-editor.js
+++ tests/__mocks__/react-filerobot-image-editor.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
 const MockedImageEditor = ({ onSave, onClose }) => {
     return (
         <div data-testid="filerobot-image-editor">
@@ -5,6 +8,11 @@ const MockedImageEditor = ({ onSave, onClose }) => {
             <button onClick={onClose}>Close</button>
         </div>
     );
+};
+
+MockedImageEditor.propTypes = {
+    onSave: PropTypes.func.isRequired,
+    onClose: PropTypes.func.isRequired,
 };
 
 export default MockedImageEditor;
