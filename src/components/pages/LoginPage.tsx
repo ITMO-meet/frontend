adf236e60554c514f49789fb9a66c4d965ee0991
artemijr <Arrtemij96@gmail.com>
2024-12-10 04:36:21 +0300

    Added api call, added logic for registration depending on user's existence

--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -1,66 +1,67 @@
-import React, { useState } from 'react';
-import { Box, Typography, Snackbar, Alert } from '@mui/material';
+import React, {useState} from 'react';
+import {Box, Typography} from '@mui/material';
 import InputText from '../basic/InputText';
 import RoundButton from '../basic/RoundButton';
-import { useNavigate } from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
+import {loginUser} from '../../api/auth';
+import {useError} from '../../contexts/ErrorContext';
 
 export const LoginPage: React.FC = () => {
-  const navigate = useNavigate();
-  const [userId, setUserId] = useState('');
-  const [password, setPassword] = useState('');
-  const [openSnackbar, setOpenSnackbar] = useState(false);
-  const [alertMessage, setAlertMessage] = useState('');
+    const navigate = useNavigate();
+    const {showError} = useError();
+    const [userId, setUserId] = useState('');
+    const [password, setPassword] = useState('');
 
-  const handleSubmit = () => {
-    if (userId.length !== 6) {
-      makeAlert('ID must be exact 6 symbols');
-      return;
-    }
+    const handleSubmit = async () => {
+        if (userId.length !== 6) {
+            showError('ID must be exactly 6 symbols');
+            return;
+        }
 
-    if (password.trim() === '') {
-      makeAlert('Password must not be empty');
-      return;
-    }
-    // TODO: check if id + password correct
-    // TODO: check if user should complete registration -> navigate("/registration")
+        if (password.trim() === '') {
+            showError('Password must not be empty');
+            return;
+        }
 
-    navigate("/chats");
-  };
+        try {
+            const {redirectUrl, isu} = await loginUser(userId, password);
 
-  const makeAlert = (message: string) => {
-    setAlertMessage(message);
-    setOpenSnackbar(true);
-  }
+            localStorage.setItem('isu', isu.toString());
 
-  const handleCloseSnackbar = () => {
-    setOpenSnackbar(false);
-  };
+            if (redirectUrl.endsWith('/auth/dashboard')) {
+                navigate("/chats");
+            } else if (redirectUrl.endsWith('/auth/register/select_username')) {
+                navigate("/register");
+            } else {
+                showError('Unexpected redirect from server');
+            }
 
-  return (
-    <Box
-      sx={{
-        height: "95hv",
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        textAlign: 'center',
-        padding: '20px',
-      }}
-    >
-      <Typography variant="h4" sx={{ marginBottom: '200px' }}>
-        Login with ITMO.ID
-      </Typography>
-      <InputText width='80%' label='ID' onChange={(e) => setUserId(e.target.value)} sx={{ marginBottom: '20px' }} />
-      <InputText width="80%" label="Password" onChange={(e) => setPassword(e.target.value)} type="password" sx={{ marginBottom: '20px' }} />
-      <RoundButton onClick={handleSubmit}>Continue</RoundButton>
-      <Snackbar open={openSnackbar} autoHideDuration={6000} onClose={handleCloseSnackbar}>
-        <Alert onClose={handleCloseSnackbar} severity="error" sx={{ width: '100%' }}>
-          {alertMessage}
-        </Alert>
-      </Snackbar>
-    </Box>
-  );
+        } catch (e: any) {
+            showError(e.message);
+        }
+    };
+
+    return (
+        <Box
+            sx={{
+                height: "95hv",
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                textAlign: 'center',
+                padding: '20px',
+            }}
+        >
+            <Typography variant="h4" sx={{marginBottom: '200px'}}>
+                Login with ITMO.ID
+            </Typography>
+            <InputText width='80%' label='ID' onChange={(e) => setUserId(e.target.value)} sx={{marginBottom: '20px'}}/>
+            <InputText width="80%" label="Password" onChange={(e) => setPassword(e.target.value)} type="password"
+                       sx={{marginBottom: '20px'}}/>
+            <RoundButton onClick={handleSubmit}>Continue</RoundButton>
+        </Box>
+    );
 };
 
 export default LoginPage;
