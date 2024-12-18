06eb71376571f671b2f397d63a9c9175d6311ec1
Kosty Manuhin <kostymanuhin@gmail.com>
2024-12-19 00:30:49 +0300

    matches

--- tests/e2e/matches.spec.ts
+++ tests/e2e/matches.spec.ts
@@ -5,6 +5,66 @@ import { expect } from '@playwright/test';
 describe('Feed page', function() {
     beforeEach(async function() {
         await makeLogin(page);
-        await page.click("text=Profile"); 
+        await page.click("text=Matches"); 
+
+        // check url
+        let url = await page.url(); 
+        await expect(url).toContain("/matches");
+    });
+
+    it('Test matches and premium', async function() {
+        await expect(await page.locator("text=Метчи разблокируются после покупки премиума.")).toHaveCount(1);
+        await page.click("text=Просмотреть план", {timeout: 5000})
+        
+        // redirect to premium
+        let url = await page.url(); 
+        await expect(url).toContain("/premium");
+        
+        // buy premium
+        await expect(await page.locator("text=Это премиум")).toHaveCount(1);
+        await page.click("text=Купить премиум", {timeout: 5000})
+        
+        // redirect to profile
+        url = await page.url(); 
+        await expect(url).toContain("/profile");
+        
+        // access granted
+        await expect(await page.locator("text=У вас премиум-аккаунт!")).toHaveCount(1);
+        await page.click("text=Matches", {timeout: 5000})
+
+        // redirect to profile
+        url = await page.url(); 
+        await expect(url).toContain("/matches");
+
+        // we have access
+        await expect(await page.locator("text=Метчи разблокируются после покупки премиума.")).toHaveCount(0);
+    });
+
+    it('Test navigation', async function() {
+        await expect(await page.locator("text=Метчи разблокируются после покупки премиума.")).toHaveCount(1);
+        await page.click("text=Просмотреть план", {timeout: 5000})
+        
+        // redirect to premium
+        let url = await page.url(); 
+        await expect(url).toContain("/premium");
+        
+        // don't buy premium
+        await expect(await page.locator("text=Это премиум")).toHaveCount(1);
+        await page.click('[data-testid="WestIcon"]', {timeout: 5000})
+        
+        // redirect to profile
+        url = await page.url(); 
+        await expect(url).toContain("/profile");
+        
+        // access not granted
+        await expect(await page.locator("text=У вас премиум-аккаунт!")).toHaveCount(0);
+        await page.click("text=Matches", {timeout: 5000})
+
+        // redirect to profile
+        url = await page.url(); 
+        await expect(url).toContain("/matches");
+
+        // we don't have access
+        await expect(await page.locator("text=Метчи разблокируются после покупки премиума.")).toHaveCount(1);
     });
 });
