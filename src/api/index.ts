7cca3fb411ff534021f40f82e8aca741e3d97a2a
OverFitted <kozak.bor@gmail.com>
2025-01-16 02:39:50 +0300

    Fix: nginx api reverse proxy

--- src/api/index.ts
+++ src/api/index.ts
@@ -6,7 +6,8 @@
 const AUTH_BASE_URL = 'http://185.178.47.42:3000'
 //const AUTH_BASE_URL = 'http://127.0.0.1:3000'
 // const TEST_BASE_URL = 'http://185.178.47.42:7000'
-const BASE_URL = 'http://185.178.47.42:8000';
+// const BASE_URL = 'http://185.178.47.42:8000';
+const BASE_URL = '/api';
 //const BASE_URL = 'http://127.0.0.1:8000';
 // const BASE_URL = 'http://127.0.0.1:28000';
 
