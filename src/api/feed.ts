1e32b685f2aff3d97bc782083f76a6d0e7771a7e
graevsky <graevsky@bk.ru>
2025-01-16 17:26:41 +0300

    Fix filters with backend

--- src/api/feed.ts
+++ src/api/feed.ts
@@ -2,11 +2,36 @@ import { getJson, postJson } from ".";
 import { DislikeResponse, LikeResponse, SuperLikeResponse, UserAction } from "../types";
 import { Profile } from "./profile";
 
-export async function getRandomPerson(isu: number) {
-    const response = await getJson<{ profile: Profile }>(`/matches/random_person?user_id=${isu}`);
-    response.profile.photos = response.profile.photos.map(photo => photo.replace("http://", "").replace("185.178.47.42:9000", "https://itmomeet.ru"));
-    response.profile.logo = response.profile.logo.replace("http://", "").replace("185.178.47.42:9000", "https://itmomeet.ru");
-    return response.profile;
+
+export async function getFilteredPerson(
+    user_id: number,
+    gender: string,
+    ageRange: number[],
+    heightRange: number[],
+    relationshipPreferences: string[]
+): Promise<{ profile: Profile }> {
+    const params = new URLSearchParams();
+    params.append("user_id", String(user_id));
+
+    if (gender) {
+        params.append("gender", gender);
+    }
+
+    if (ageRange?.length === 2) {
+        params.append("min_age", String(ageRange[0]));
+        params.append("max_age", String(ageRange[1]));
+    }
+
+    if (heightRange?.length === 2) {
+        params.append("min_height", String(heightRange[0]));
+        params.append("max_height", String(heightRange[1]));
+    }
+
+    relationshipPreferences.forEach((relPref) => {
+        params.append("relationship_preferences", relPref);
+    });
+
+    return await getJson<{ profile: Profile }>(`/matches/random_persona?${params.toString()}`);
 }
 
 export async function likePerson(user_id: number, target_id: number): Promise<LikeResponse> {
