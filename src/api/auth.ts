bf355a3e595aadcc45663dc2cbb642a7b80f6226
Kosty Manuhin <kostymanuhin@gmail.com>
2025-01-16 15:17:17 +0300

    wrong login

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -4,7 +4,7 @@
 
 import { postJson } from './index';
 
-export async function loginUser(username: string, password: string): Promise<{ redirectUrl: string, isu: number }> {
+export async function loginUser(username: string, password: string): Promise<{ redirectUrl?: string, isu?: number, detail?: string }> {
     const data = await postJson<{ redirect: string, isu: number }>('/auth/login_with_password', { username, password });
     return { redirectUrl: data.redirect, isu: data.isu };
 }
